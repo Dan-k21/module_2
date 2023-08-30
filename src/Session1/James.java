941cc50e1da9609cefeb4a264c8a4d7134c29969
Dân <138743748+Dan-k21@users.noreply.github.com>
2023-08-30 14:54:22 +0700

    bai tap

---
+++ src/Session1/James.java
@@ -0,0 +1,16 @@
+package Session1;
+
+import java.util.Scanner;
+
+public class James {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Chuyển đổi tiền tệ
+        double vnd = 23000;
+        System.out.print("USD: ");
+        double money = input.nextDouble();
+        double usd = money * vnd;
+        System.out.println("VND: " + usd);
+
+    }
+}
