243e7beba297756b6297cfadf93bb12de5868791
Dân <138743748+Dan-k21@users.noreply.github.com>
2023-09-05 22:54:51 +0700

    bai tap

--- src/Session1/James.java
+++ src/Session1/James.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class James {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        // Chuyển đổi tiền tệ
-        double vnd = 23000;
-        System.out.print("USD: ");
-        double money = input.nextDouble();
-        double usd = money * vnd;
-        System.out.println("VND: " + usd);
+//        Chuyển đổi tiền tệ
+//        double vnd = 23000;
+//        System.out.print("USD: ");
+//        double money = input.nextDouble();
+//        double usd = money * vnd;
+//        System.out.println("VND: " + usd);
+//        Chữ thành số
+        System.out.println("");
+
+
+
+
     }
 }
---
+++ src/Session2/James.java
@@ -0,0 +1,135 @@
+package Session2;
+
+import java.util.Scanner;
+
+public class James {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        Kiểm tra số nguyên tố
+//        System.out.println("Nhập số: ");
+//        int num = input.nextInt();
+//        int count = 0;
+//        for (int i = 1; i <= num; i++) {
+//            if (num % i == 0) {
+//                count++;
+//            }
+//        }
+//        if (count == 2) {
+//            System.out.println("Đây là số nguyên tố");
+//        } else {
+//            System.out.println("Đây không là số nguyên tố");
+//        }
+//        Tính tiền lãi cho vay
+//        System.out.println("Số tiền cho vay: ");
+//        Double money = input.nextDouble();
+//        System.out.println("Tỉ lệ lãi suất theo tháng: ");
+//        Double interestRate = input.nextDouble();
+//        System.out.println("Số tháng cho vay: ");
+//        Double month = input.nextDouble();
+//        Double interest = money * (interestRate / 12) * month;
+//        System.out.println("Số tiền lãi là: " + interest);
+//        Ước chung lớn nhất của 2 số
+//        System.out.println("Nhập a: ");
+//        int a = Math.abs(input.nextInt());
+//        System.out.println("Nhập b: ");
+//        int b = Math.abs(input.nextInt());
+//        if (a == 0 || b == 0) {
+//            System.out.println("Không có");
+//        }
+//        while (a != b) {
+//            if (a > b) {
+//                a -= b;
+//            } else {
+//                b -= a;
+//            }
+//        }
+//        System.out.println("Ước chung lớn nhất của 2 số là: " + a);
+//        Thiết kế menu
+//        int choice = -1;
+//        while (choice != 0) {
+//            System.out.println("Thích gì?");
+//            System.out.println("1. Hình vuông");
+//            System.out.println("2. Hình chữ nhật");
+//            System.out.println("3. Hình tam giác");
+//            choice = input.nextInt();
+//            switch (choice) {
+//                case 1:
+//                    System.out.println("Hình vuông");
+//                    System.out.println("******");
+//                    System.out.println("******");
+//                    System.out.println("******");
+//                    break;
+//                case 2:
+//                    System.out.println("Hình chữ nhật");
+//                    System.out.println("**********");
+//                    System.out.println("**********");
+//                    System.out.println("**********");
+//                    break;
+//                case 3:
+//                    System.out.println("Hình tam giác");
+//                    System.out.println("*");
+//                    System.out.println("**");
+//                    System.out.println("***");
+//                    break;
+//                default:
+//                    System.out.println("Không có");
+//            }
+//        }
+        // Hiển thị các loại hình
+//        int choice = -1;
+//        while (choice != 0) {
+//            System.out.println("Menu: ");
+//            System.out.println("1. Hình chữ nhật");
+//            System.out.println("2. Tam giác vuông");
+//            System.out.println("3. Tam giác cân");
+//            choice = input.nextInt();
+//            switch (choice) {
+//                case 1:
+//                    System.out.println("Hình chữ nhật");
+//                    for (int i = 0; i < 3; i++) {
+//                        for (int j = 0; j < 7; j++) {
+//                            System.out.print("* ");
+//                        }
+//                        System.out.println();
+//                    }
+//                    break;
+//                case 2:
+//                    System.out.println("Tam giác vuông");
+//                    for (int i = 0; i < 7; i++) {
+//                        for (int j = 0; j < i; j++) {
+//                            System.out.print("* ");
+//                        }
+//                        System.out.println();
+//                    }
+//                    break;
+//                case 3:
+//                    System.out.println("Tam giác cân");
+//                    for (int i = 1; i < 8; i++) {
+//                        for (int j = 1; j < 8 - i; j++) {
+//                            System.out.print(" ");
+//                        }
+//                        for (int j = 1; j <= 2 * i - 1; j++) {
+//                            System.out.print("*");
+//                        }
+//                        System.out.println();
+//                    }
+//                default:
+//                    System.out.println("Không có");
+//            }
+//        }
+//        System.out.println("Nhập số: ");
+//        int quantity = input.nextInt();
+//        int n = 2;
+//        int count = 0;
+//        for (int i = 0; i < ; i++) {
+//            for (int j = 1; j <= n; j++, n++) {
+//                if (n % j == 0) {
+//                    count++;
+//                }
+//            }
+//            if (count == 2) {
+//                System.out.println(n);
+//            }
+//        }
+    }
+}
